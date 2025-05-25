2607b638b8211261cb2a8b0009ac3032ad11f934
mirfannsaputraa21 <m.irfansaputra0089@gmail.com>
2025-05-25 22:49:58 +0700

    fix coding eror 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../halaman/Home.vue'
+import home from '../halaman/home.vue'
 import Login from '../halaman/Login.vue'
 import NotFound from '../halaman/NotFound.vue'
 import Pemesanan from '../halaman/Pemesanan.vue'
 import Contact from '../halaman/Contact.vue'
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: home },
   { path: '/login', component: Login },
   { path: '/pemesanan', component: Pemesanan },
   { path: '/contact', component: Contact },
