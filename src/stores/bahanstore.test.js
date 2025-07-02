300c5b5bc523c325118a73db8211ba9d7483438a
mirfannsaputraa21 <m.irfansaputra0089@gmail.com>
2025-07-02 18:50:54 +0700

    menambahkan json server

--- src/stores/bahanstore.test.js
+++ src/stores/bahanstore.test.js
@@ -3,7 +3,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/bahan';
+const API_URL = 'https://irfan-json-server.glitch.me/bahan';
 
 export const useBahanStore = defineStore('bahan', {
   state: () => ({
